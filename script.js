1e4055cda38959ccd610637110c4656c896db6f0
Bagas Dwi Anggoro <shirrogannie@gmail.com>
2023-10-31 06:34:01 +0700

    Update layout

--- script.js
+++ script.js
@@ -10,50 +10,50 @@ window.addEventListener('contextmenu', function (e) {
     e.preventDefault(); 
 }, false);
 
-/// navigation menu toggle
+//mask
+function da(){
+    document.getElementById("mask").style.top = "32%"
+}
+function db(){
+    document.getElementById("mask").style.top = "58%"
+}
+function dc(){
+    document.getElementById("mask").style.top = "100%"
+}
+
+//nav
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+    var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+        document.getElementById("nav").style.top = "0";
+    } else {
+        document.getElementById("nav").style.top = "-40px";
+    }
+    prevScrollpos = currentScrollPos;
+}
+
 function openNav() {
-    var x = document.getElementById('rightCol');
+    var x = document.getElementById('navChild');
     if (x.style.display === "flex") {
         setTimeout(function () {
             x.style.display = "none";
-            fadeIn();
         }, 0)
     } else {
         setTimeout(function () {
             x.style.display = "flex";
-            fadeOut();
-        }, 300)
-    }
-
-    function fadeIn(){
-        x.style.animation= "fadeIn 0.3s ease-in-out";
-        }
-    
-    function fadeOut(){
-        x.style.animation= "fadeOut 0.3s ease-in-out";
+        }, 0)
     }
 }
 
-var w = document.documentElement.clientWidth || window.innerWidth;
-if (w <= 719) {
-// Probably mobile
-    function closeNav(){
-        document.getElementById("rightCol").style.display = "none";
-        document.getElementById("ham").element.classList.remove(".active");
-        var element = document.getElementsById("ham");
-        element.classList.remove(".active");
+//checkbox
+function bg() {
+    var checkbox = document.getElementById("eax");
+    var bg = document.getElementById("sample");
+
+    if (checkbox.checked == true) {
+        bg.style.backgroundImage = "url('Assets/bg.webp')";
+    } else {
+        bg.style.backgroundImage = "url('')";
     }
-} else {
-// Probably desktop
-function closeNav(){
-    document.getElementById("rightCol").style.display = "";
-}
-}
-
-let loader=document.querySelector('.preLoader');
-
-window.addEventListener("load",preLoader);
-
-function preLoader(){
-    loader.classList.add(".preLoader")
 }
